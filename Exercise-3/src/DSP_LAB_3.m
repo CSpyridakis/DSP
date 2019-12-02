1f15b8aa6fdf2df6f01f75fb201f00959ad8e2ff
Spyridakis Christos <spyridakischristos@gmail.com>
2019-12-02 11:57:07 +0200

    Exec 3: 3) Frequency spectrum updated

--- Exercise-3/src/DSP_LAB_3.m
+++ Exercise-3/src/DSP_LAB_3.m
@@ -7,7 +7,7 @@
 %               - Paterakis Isidoros
 %
 %   Created Date : 27/11/2019
-%   Last Updated : 1/12/2019
+%   Last Updated : 2/12/2019
 %
 %   Description: 
 %               Code created for labs of Digital Signal Processing Course
@@ -113,24 +113,24 @@ y2 = filter(NUM2, DEN2, x_a);               % attenuation = 50db
 
 % X(F) Fourier Transform of x(t)
 F = -Fs/2 : Fs/(number_of_samples) : (Fs/2) - (Fs/number_of_samples);
-X_a = abs(fftshift(fft(x_a)*Ts)).^2;
+X_a = abs(fftshift(fft(x_a)*Ts));
 
 % Filters Fourier
-Y1 = abs(fftshift(fft(y1)*Ts)).^2;
-Y2 = abs(fftshift(fft(y2)*Ts)).^2;
+Y1 = abs(fftshift(fft(y1)*Ts));
+Y2 = abs(fftshift(fft(y2)*Ts));
 
 % Display them
 figure(); 
-subplot(3,1,1); plot(n, x_a, 'b', n, x_a, 'r.'); legend('Reconstructed','Samples'); legend('Location','NorthEast'); title('x(t)'); xlabel('n'); ylabel('A');
-subplot(3,1,2); plot(n, y1, 'b', n, y1, 'r.'); legend('Reconstructed','Samples'); legend('Location','NorthEast'); title('x(t) filtered attenuation = 30db'); xlabel('n'); ylabel('A');
-subplot(3,1,3); plot(n, y2, 'b', n, y2, 'r.'); legend('Reconstructed','Samples'); legend('Location','NorthEast'); title('x(t) filtered attenuation = 50db'); xlabel('n'); ylabel('A');
+subplot(3,1,1); plot(n, x_a, 'b', n, x_a, 'r.'); legend('Reconstructed','Samples'); legend('Location','NorthEast'); title('x(t)'); xlabel('n'); ylabel('Amplitude');
+subplot(3,1,2); plot(n, y1, 'b', n, y1, 'r.'); legend('Reconstructed','Samples'); legend('Location','NorthEast'); title('x(t) filtered attenuation = 30db'); xlabel('n'); ylabel('Amplitude');
+subplot(3,1,3); plot(n, y2, 'b', n, y2, 'r.'); legend('Reconstructed','Samples'); legend('Location','NorthEast'); title('x(t) filtered attenuation = 50db'); xlabel('n'); ylabel('Amplitude');
 
 figure(); 
-subplot(3,1,1); plot(F, X_a, 'b'); title('|X(F)|^2'); xlabel('F'); ylabel('x(t)');
-subplot(3,1,2); plot(F, Y1, 'b'); title('|X(F)|^2 filtered attenuation = 30db'); xlabel('F'); ylabel('A');
-subplot(3,1,3); plot(F, Y2, 'b'); title('|X(F)|^2 filtered attenuation = 50db'); xlabel('F'); ylabel('A');
+subplot(3,1,1); plot(F, X_a, 'b'); title('Frequency spectrum of x(t) - (|X(F)|)'); xlabel('F'); ylabel('Magnitude');
+subplot(3,1,2); plot(F, Y1, 'b'); title('Frequency spectrum of x(t) - (|X(F)|) filtered attenuation = 30db'); xlabel('F'); ylabel('Magnitude');
+subplot(3,1,3); plot(F, Y2, 'b'); title('Frequency spectrum of x(t) - (|X(F)|) filtered attenuation = 50db'); xlabel('F'); ylabel('Magnitude');
     
-%3a endsattenuation = 30db
+%3a end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -149,19 +149,19 @@ y3 = filter(b2,a2,x_b);
 
 % X(F) Fourier Transform of x(t)
 F = -Fs/2 : Fs/(number_of_samples) : (Fs/2) - (Fs/number_of_samples);
-X_b = abs(fftshift(fft(x_b)*Ts)).^2;
+X_b = abs(fftshift(fft(x_b)*Ts));
 
 % Filter's Fourier
-Y3 = abs(fftshift(fft(y3)*Ts)).^2;
+Y3 = abs(fftshift(fft(y3)*Ts));
 
 % Display them
 figure(); 
-subplot(2,1,1); plot(n, x_b, 'b', n, x_b, 'r.'); legend('Reconstructed','Samples'); legend('Location','NorthEast'); title('x(t)'); xlabel('n'); ylabel('A');
-subplot(2,1,2); plot(n, y3, 'b', n, y3, 'r.'); legend('Reconstructed','Samples'); legend('Location','NorthEast'); title('x(t) filtered'); xlabel('n'); ylabel('A');
+subplot(2,1,1); plot(n, x_b, 'b', n, x_b, 'r.'); legend('Reconstructed','Samples'); legend('Location','NorthEast'); title('x(t)'); xlabel('n'); ylabel('Amplitude');
+subplot(2,1,2); plot(n, y3, 'b', n, y3, 'r.'); legend('Reconstructed','Samples'); legend('Location','NorthEast'); title('x(t) filtered'); xlabel('n'); ylabel('Amplitude');
 
 figure(); 
-subplot(2,1,1); plot(F, X_b, 'b'); title('|X(F)|^2'); xlabel('F'); ylabel('A');
-subplot(2,1,2); plot(F, Y3, 'b'); title('|X(F)|^2 filtered'); xlabel('F'); ylabel('A');
+subplot(2,1,1); plot(F, X_b, 'b'); title('Frequency spectrum of x(t) - (|X(F)|)'); xlabel('F'); ylabel('Magnitude');
+subplot(2,1,2); plot(F, Y3, 'b'); title('Frequency spectrum of x(t) - (|X(F)|) filtered'); xlabel('F'); ylabel('Magnitude');
 
 
 %3b ends
